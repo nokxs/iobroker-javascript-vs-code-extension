a77836eb27e6da693452a0d94a179ffe00ee609c
Niklas <niklas@nokxs.de>
2021-01-07 21:57:27 +0100

    No auto connect on startup

--- src/Startup.ts
+++ src/Startup.ts
@@ -4,20 +4,16 @@ import TYPES from "./Types";
 import { ICommandService } from "./services/command/ICommandService";
 import { commands, ExtensionContext, window } from "vscode";
 import { ScriptExplorerProvider } from "./views/scriptExplorer/ScriptExplorerProvider";
-import { IIobrokerConnectionService } from "./services/iobrokerConnection/IIobrokerConnectionService";
 
 @injectable()
 export class Startup implements IStartup {
     constructor(
         @inject(TYPES.services.command) private commandService: ICommandService,
-        @inject(TYPES.services.iobrokerConnection) private iobrokerConnectionService: IIobrokerConnectionService,
         @inject(TYPES.views.scriptExplorer) private scriptExplorerProvider: ScriptExplorerProvider
     ) {}
 
     async init(context: ExtensionContext): Promise<void> {
         this.commandService.registerCommands(context);
-        
-        await this.iobrokerConnectionService.connect();
         
         // TODO: Move to own service
         window.registerTreeDataProvider("iobroker-javascript.script-explorer", this.scriptExplorerProvider);
