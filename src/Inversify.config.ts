87b736a68d6e73e131919f6ebeb4666e73a07502
Niklas <niklas@nokxs.de>
2021-12-17 15:33:06 +0100

    add view which shows all files which differ on the local and remote

--- src/Inversify.config.ts
+++ src/Inversify.config.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import { AdminVersionDetector } from './services/adminVersionDetector/AdminVersionDetector';
 import { ChangeJsInstanceCommand } from './commands/ChangeJsInstanceCommand';
+import { ChangedFilesProvider } from './views/changedFiles/ChangedFilesProvider';
 import { CommandService } from './services/command/CommandService';
 import { ConfigCreationService } from './services/configCreation/ConfigCreationService';
 import { ConfigRepositoryService } from './services/configRepository/ConfigRepositoryService';
@@ -19,6 +20,7 @@ import { DownloadAllCommand } from './commands/DownloadAllCommand';
 import { DownloadCommand } from './commands/DownloadCommand';
 import { FileService } from './services/file/FileService';
 import { IAdminVersionDetector } from './services/adminVersionDetector/IAdminVersionDetector';
+import { IChangedFilesProvider } from './views/changedFiles/IChangedFilesProvider';
 import { ICommand } from './commands/ICommand';
 import { ICommandService } from './services/command/ICommandService';
 import { IConfigCreationService } from './services/configCreation/IConfigCreationService';
@@ -107,5 +109,6 @@ container.bind<ICommand>(TYPES.command).to(CreateTypeScriptFileCommandy);
 container.bind<ICommand>(TYPES.command).to(CreateJavaScriptFileCommand);
 
 container.bind<IScriptExplorerProvider>(TYPES.views.scriptExplorer).to(ScriptExplorerProvider).inSingletonScope();
+container.bind<IChangedFilesProvider>(TYPES.views.changedFiles).to(ChangedFilesProvider).inSingletonScope();
 
 export default container;
