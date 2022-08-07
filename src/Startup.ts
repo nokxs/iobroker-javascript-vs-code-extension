e05cfd3cfc0afd731d8195b9e357b878003a058c
Niklas <niklas@nokxs.de>
2022-08-07 20:38:52 +0200

    Debug log can be created directly on startup

--- src/Startup.ts
+++ src/Startup.ts
@@ -7,6 +7,8 @@ import { ScriptExplorerProvider } from "./views/scriptExplorer/ScriptExplorerPro
 import { IIobrokerConnectionService } from "./services/iobrokerConnection/IIobrokerConnectionService";
 import { IWorkspaceService } from "./services/workspace/IWorkspaceService";
 import { ChangedScriptsProvider as ChangedScriptsProvider } from "./views/changedScripts/ChangedScriptsProvider";
+import { IConfigRepositoryService } from "./services/configRepository/IConfigRepositoryService";
+import { IDebugLogService } from "./services/debugLogService/IDebugLogService";
 
 @injectable()
 export class Startup implements IStartup {
@@ -15,7 +17,9 @@ export class Startup implements IStartup {
         @inject(TYPES.services.iobrokerConnection) private iobrokerConnectionService: IIobrokerConnectionService,
         @inject(TYPES.services.workspace) private workSpaceService: IWorkspaceService,
         @inject(TYPES.views.scriptExplorer) private scriptExplorerProvider: ScriptExplorerProvider,
-        @inject(TYPES.views.changedScripts) private changedScriptsProvider: ChangedScriptsProvider
+        @inject(TYPES.views.changedScripts) private changedScriptsProvider: ChangedScriptsProvider,
+        @inject(TYPES.services.configRepository) private configRepositoryService: IConfigRepositoryService,
+        @inject(TYPES.services.debugLogService) private debugLogService: IDebugLogService
     ) {}
 
     async init(context: ExtensionContext): Promise<void> {
@@ -23,6 +27,11 @@ export class Startup implements IStartup {
         
         const workspacesWithConfig = await this.workSpaceService.getWorkspacesWithConfig();
         if (workspacesWithConfig.length === 1) {
+            const config = await this.configRepositoryService.read(workspacesWithConfig[0]);
+            if (config.collectDebugLog) {
+                this.debugLogService.startCollecting();
+            }
+
             await this.iobrokerConnectionService.connect();
         } else if (workspacesWithConfig.length > 1) {
             window.setStatusBarMessage("ioBroker: No auto connect possible. Multiple 'iobroker-config.json' found.");
