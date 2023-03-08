4f04067e9e2ca65e682ed9faa7fadfed1c75364e
Niklas <niklas@nokxs.de>
2023-03-08 21:18:52 +0100

    Implement file check in js creation command

--- src/commands/CreateJavaScriptFileCommand.ts
+++ src/commands/CreateJavaScriptFileCommand.ts
@@ -7,6 +7,8 @@ import { ScriptDirectory } from "../views/scriptExplorer/ScriptDirectory";
 import { Uri, window } from "vscode";
 import { EngineType } from "../models/EngineType";
 import { IScriptService } from "../services/script/IScriptService";
+import { IScript } from "../models/IScript";
+import { IScriptRepositoryService } from "../services/scriptRepository/IScriptRepositoryService";
 
 @injectable()
 export class CreateJavaScriptFileCommand implements ICommand {
@@ -14,6 +16,7 @@ export class CreateJavaScriptFileCommand implements ICommand {
 
     constructor(        
         @inject(TYPES.services.scriptRemote) private scriptRemoteService: IScriptRemoteService,
+        @inject(TYPES.services.scriptRepository) private scriptRepositoryService: IScriptRepositoryService,
         @inject(TYPES.services.script) private scriptService: IScriptService,
         @inject(TYPES.services.file) private fileService: IFileService
     ) {}
@@ -29,11 +32,38 @@ export class CreateJavaScriptFileCommand implements ICommand {
                 }
 
                 const script = this.scriptService.getDefaultScript(`${scriptDirectory.directory._id}.${fileName}`, EngineType.javascript);
-                this.scriptRemoteService.uploadScript(script);
-
                 const fileUri = Uri.joinPath(scriptDirectory.directory.absoluteUri, `${fileName}.js`);
+
+                if(!await this.shouldFileReallyBeCreated(script, fileUri)) {
+                    return;
+                }
+
+                await this.scriptRemoteService.uploadScript(script);
                 await this.fileService.saveToFile(fileUri, "");
             }
         }
+    }
+    
+    private async shouldFileReallyBeCreated(script: IScript, fileUri: Uri): Promise<boolean> {
+        const existingScriptOnServer = this.scriptRepositoryService.getScriptFromId(script._id);
+        if (existingScriptOnServer) {
+            const result = await this.askUserIfFileShouldBeOverriden(script);
+            if (result === "No") {
+                return false;
+            }
+        }
+        
+        if (this.fileService.fileExists(fileUri)) {
+            const result = await this.askUserIfFileShouldBeOverriden(script);
+            if (result === "No") {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private async askUserIfFileShouldBeOverriden(script: IScript) {
+        return await window.showQuickPick(["No", "Yes"], { canPickMany: false, placeHolder: `'${script._id}' exists already. Override?` });
     }
 }
