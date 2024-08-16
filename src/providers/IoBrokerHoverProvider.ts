d367add9ca8704add342a9492eadca8c73e78c73
Niklas <niklas@nokxs.de>
2024-08-16 16:46:31 +0200

    Add more infos to tooltip

--- src/providers/IoBrokerHoverProvider.ts
+++ src/providers/IoBrokerHoverProvider.ts
@@ -3,11 +3,13 @@ import { CancellationToken, Hover, Position, TextDocument } from "vscode";
 import { IStateAndObjectRemoteService } from "../services/stateRemote/IStateAndObjectRemoteService";
 import TYPES from "../Types";
 import { IIobrokerHoverProvider } from "./IIobrokerHoverProvider";
+import { IObjectRepositoryService } from "../services/StateRepository/IObjectRepositoryService";
 
 @injectable()
 export class IoBrokerHoverProvider implements IIobrokerHoverProvider {
     constructor(
         @inject(TYPES.services.stateAndObjectRemoteService) private stateAndObjectRemoteService: IStateAndObjectRemoteService,
+        @inject(TYPES.services.objectRepositoryService) private objectRepositoryService: IObjectRepositoryService
     ) { }
 
     async provideHover(document: TextDocument, position: Position, token: CancellationToken): Promise<Hover | undefined> {
@@ -20,13 +22,21 @@ export class IoBrokerHoverProvider implements IIobrokerHoverProvider {
             if (!state || token.isCancellationRequested) {
                 return undefined;
             }
+
+            const iobrokerObject = this.objectRepositoryService.getObjectById(id) ?? undefined;
             
             // do not fix the formatting or tooltip will be formatted wrongly
-            return { contents: [`**${id}**\n\n
+            return { contents: [`**${id}**
+
+${iobrokerObject?.common?.name}     
+                
 - Value: *${state.val}*
 - Ack: *${state.ack}*
 - Last Changed: *${this.formatDate(state.lc)}*
-- Timestamp: *${this.formatDate(state.ts)}*`] };
+- Timestamp: *${this.formatDate(state.ts)}*
+- Read/Write: *${iobrokerObject?.common?.read ?? "unknown"}/${iobrokerObject?.common?.write ?? "unknown"}*
+- Role: *${iobrokerObject?.common?.role ?? "unknown"}*
+- Type: *${iobrokerObject?.common?.type ?? "unknown"}*`] };
         }
 
         return undefined;
