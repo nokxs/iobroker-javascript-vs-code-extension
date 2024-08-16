5753e21dadb74e1b909b33e20154b4adf2f9f594
Niklas <niklas@nokxs.de>
2024-08-16 20:31:10 +0200

    fix bug in hoverProvider

--- src/providers/IoBrokerCompletionItemProvider.ts
+++ src/providers/IoBrokerCompletionItemProvider.ts
@@ -16,7 +16,7 @@ export class IoBrokerCompletionItemProvider implements IIobrokerCompletionItemPr
         document: TextDocument,
         position: Position,
         token: CancellationToken): Promise<CompletionItem[] | undefined> {
-        const wordRange = document.getWordRangeAtPosition(position, /["'`].*["'`]/);
+        const wordRange = document.getWordRangeAtPosition(position, /["'`].*?["'`]/);
         if (wordRange) {
             // slice removes first and last char
             const text = document.getText(wordRange).slice(1, position.character - wordRange.start.character);
