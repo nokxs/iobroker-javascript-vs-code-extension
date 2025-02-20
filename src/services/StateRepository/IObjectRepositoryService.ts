639bed5ea6395f2062569819fefd3a288fb4ee53
Niklas <niklas@nokxs.de>
2025-02-20 22:07:58 +0100

    Improve finding objects via object repository service

--- src/services/StateRepository/IObjectRepositoryService.ts
+++ src/services/StateRepository/IObjectRepositoryService.ts
@@ -3,6 +3,6 @@ import { IObjectList } from "../../models/IObjectList";
 
 export interface IObjectRepositoryService {
     init(): Promise<void>;
-    findMatchingObjects(partialId: string): IObjectList | undefined;
+    findMatchingObjectsByPartialIdAndName(partialId: string): IObjectList | undefined;
     getObjectById(id: string): IObject | undefined;
 }
